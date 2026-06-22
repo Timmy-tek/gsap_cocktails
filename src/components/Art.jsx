089277819b946878ef72316db68e0a01e7551b43
Adegbola Timothy <adegbolatimothyjosh@gmail.com>
2026-06-22 16:09:36 +0100

    Implemented the contact section

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -63,7 +63,7 @@ const Art = () => {
 
                 <div className="masked-container">
                     <h2 className="will-fade">Sip-Worthy Perfection</h2>
-                    <div id="masked-content">
+                    <div id="masked-content" className="mt-22">
                         <h3>Made with Craft, Poured with Passion</h3>
                         <p>This isn’t just a drink. It’s a carefully crafted moment made just for you.</p>
                     </div>
